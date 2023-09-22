b8dd0710de0eac370a1c99169e8ba6fe4163b1a2
Visten_Home <Vlado@DESCTOP_ZEROTWO>
2023-09-23 02:29:43 +0300

    Lololoshka!!

--- Условный-Оператор/Вложенные и каскадные условия/main 4.3-9.py
+++ Условный-Оператор/Вложенные и каскадные условия/main 4.3-9.py
@@ -1,8 +1,15 @@
 a1, b1, a2, b2 = int(input()), int(input()), int(input()), int(input())
 
-if b1 == a2:
-    print(b1)
-elif a1<b1:
-    print(a2,b1)
-else:
+if a2 < a1:
+    # теперь отрезок a1, b1 точно левее (по крайней мере своим левым концом) отрезка a2, b2
+    a1, b1, a2, b2 = a2, b2, a1, b1
+
+if a2 > b1:
     print("пустое множество")
+elif a2 == b1:
+    print(a2)
+else:
+    if b1 < b2:
+        print(a2, b1)
+    else:
+        print(a2, b2)
